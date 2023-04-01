23627dbcc13a0cf6466bb095ab4ba69d7489f81e
ibimon <ibimon@gmail.com>
2023-04-01 17:26:39 -0300

    Commite listagem de livros ok

--- src/app/components/views/livros/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livros/livro-read-all/livro-read-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit} from '@angular/core';
+import { Livro } from '../livro.model';
+import { LivroService } from '../livro.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-livro-read-all',
@@ -7,9 +10,20 @@ import { Component, OnInit} from '@angular/core';
 })
 export class LivroReadAllComponent {
   displayedColumns: string[] = ['id', 'titulo', 'livros','acoes'];
+  id_cat : String = '';
+  livros: Livro[] = [];
 
-  constructor(){}
+  constructor(private service: LivroService, private route:ActivatedRoute){}
 
-  ngOnInit():void{}
+  ngOnInit():void{
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.findAll();
+  }
 
+  findAll(): void{
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta) =>{
+      this.livros = resposta;
+      console.log(this.livros);
+    });
+  }
 }
---
+++ src/app/components/views/livros/livro.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Livro } from './livro.model';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+  baseUrl: String = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+
+  findAllByCategoria(id_cat: String): Observable<Livro[]>{
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.get<Livro[]>(url);
+  }
+}
