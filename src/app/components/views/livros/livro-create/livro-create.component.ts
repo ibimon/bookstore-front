39a1192449c03e50ed57e2b05c5d9b98f328ed3f
ibimon <ibimon@gmail.com>
2023-04-01 19:01:38 -0300

    Create livos terminando create com mensagens e rotas de retorno

--- src/app/components/views/livros/livro-create/livro-create.component.ts
+++ src/app/components/views/livros/livro-create/livro-create.component.ts
@@ -1,31 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { LivroService } from "../livro.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Livro } from "../livro.model";
 
 @Component({
-  selector: 'app-livro-create',
-  templateUrl: './livro-create.component.html',
-  styleUrls: ['./livro-create.component.css']
+  selector: "app-livro-create",
+  templateUrl: "./livro-create.component.html",
+  styleUrls: ["./livro-create.component.css"],
 })
-export class LivroCreateComponent implements OnInit{
+export class LivroCreateComponent implements OnInit {
+  id_cat: String = "";
 
-  titulo = new FormControl('', [Validators.minLength(3)])
-  nome_autor = new FormControl('', [Validators.minLength(3)])
-  texto = new FormControl('', [Validators.minLength(10)])
+  livro: Livro = {
+    id: "",
+    titulo: "",
+    nomeAutor: "",
+    texto: "",
+  };
 
-  constructor(){}
+  titulo = new FormControl("", [Validators.minLength(3)]);
+  nomeAutor = new FormControl("", [Validators.minLength(3)]);
+  texto = new FormControl("", [Validators.minLength(10)]);
 
-  ngOnInit():void{}
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
+  }
 
-  getMessage(){
-    if(this.titulo.invalid){
-      return 'O campo titulo deve conter entre 3 e 100 caracteres.'
+  create(): void {
+    this.service.create(this.livro, this.id_cat).subscribe(
+      (resposta) => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.service.mensagem("Livro criado com sucesso!");
+      },
+      err => {
+        this.router.navigate([`categorias/${this.id_cat}/livros`]);
+        this.service.mensagem("Erro ao criar novo livro, tente depois!");
+      }
+    );
+  }
+
+  getMessage() {
+    if (this.titulo.invalid) {
+      return "O campo titulo deve conter entre 3 e 100 caracteres.";
     }
-    if(this.nome_autor.invalid){
-      return 'O campo nome do autor deve conter entre 3 e 100 caracteres.'
+    if (this.nomeAutor.invalid) {
+      return "O campo nome do autor deve conter entre 3 e 100 caracteres.";
     }
-    if(this.texto.invalid){
-      return 'O campo texto deve conter entre 10 e 2000000 caracteres.'
+    if (this.texto.invalid) {
+      return "O campo texto deve conter entre 10 e 2000000 caracteres.";
     }
     return false;
   }
--- src/app/components/views/livros/livro.service.ts
+++ src/app/components/views/livros/livro.service.ts
@@ -1,20 +1,32 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Livro } from './livro.model';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Livro } from "./livro.model";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LivroService {
   baseUrl: String = environment.baseUrl;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) {}
 
-  findAllByCategoria(id_cat: String): Observable<Livro[]>{
-    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+  findAllByCategoria(id_cat: String): Observable<Livro[]> {
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`;
     return this.http.get<Livro[]>(url);
   }
 
-  
+  create(livro: Livro, id_cat: String): Observable<Livro> {
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`;
+    return this.http.post<Livro>(url, livro);
+  }
+
+  mensagem(str: String): void {
+    this._snack.open(`${str}`, `OK`, {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000,
+    });
+  }
 }
